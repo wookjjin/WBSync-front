892b641fa349375ea87130a10c7faadb1c4c1381
JinWook <jwook0318@gmail.com>
2025-03-24 16:54:41 +0900

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: false },
+  modules: ['@nuxt/eslint'],
+  eslint: {
+    config: {
+      stylistic: {
+        semi: true
+      }
+    }
+  }
+})
