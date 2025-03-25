44dcfeec0fa7782468c0eadc1a359441b77b8729
JeongHun4035 <eogns4035@gmail.com>
2025-03-25 09:14:14 +0900

    feature/base-router-set

---
+++ pages/auth/sign-in.vue
@@ -0,0 +1,11 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+  <div>
+    sign in
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/auth/sign-up.vue
@@ -0,0 +1,11 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+  <div>
+    sign up
+  </div>
+</template>
+
+<style scoped></style>
