86db0f676fa8d9c05a7728981395f4f258195766
JeongHun4035 <eogns4035@gmail.com>
2025-03-25 09:47:53 +0900

    feature/api-connect

--- pages/auth/sign-in.vue
+++ pages/auth/sign-in.vue
@@ -1,11 +1,45 @@
 <script setup lang='ts'>
+const getUser = async () => {
+  try {
+    const result = await request.get('user');
+    console.log('result::', result);
+    return result;
+  }
+  catch (error) {
+    console.error(error);
+  }
+};
 
+onMounted(() => {
+  getUser();
+});
 </script>
 
 <template>
-  <div>
-    sign in
+  <div class="sign-in-container">
+    <div class="sign-in-row">
+      <span>ID</span>
+      <input type="text" placeholder="ID">
+    </div>
+    <div class="sign-in-row">
+      <span>Password</span>
+      <input type="text" placeholder="password">
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.sign-in-container {
+  display: flex;
+  flex-direction: column;
+  border: solid 1px;
+  width: 800px;
+  height: auto;
+}
+
+.sign-in-row {
+  display: flex;
+  padding: 20px;
+  justify-content: space-between;
+}
+</style>
