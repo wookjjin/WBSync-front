11a02c13ddfb28a73532438c3d8245d0ad2821fe
JeongHun4035 <eogns4035@gmail.com>
2025-03-31 15:12:06 +0900

    progress-apply

--- composables/request.ts
+++ composables/request.ts
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+import { useLoadingStore } from '~/store/progress'
+
 declare module 'axios' {
   interface AxiosRequestMeta {
     meta?: {
@@ -17,6 +19,8 @@ const instance = axios.create({
 
 instance.interceptors.request.use(
   (config) => {
+    const loadingStore = useLoadingStore()
+    loadingStore.startLoading()
     const token = localStorage.getItem('token')
     if (token) {
       config.headers.Authorization = `Bearer ${token}`
@@ -25,13 +29,22 @@ instance.interceptors.request.use(
   },
 
   (error) => {
+    const loadingStore = useLoadingStore()
+    loadingStore.stopLoading()
     return Promise.reject(error)
   }
 )
 
 instance.interceptors.response.use(
-  (response) => response,
+  (response) => {
+    const loadingStore = useLoadingStore()
+    loadingStore.stopLoading()
+    return response
+  },
   async (error) => {
+    const loadingStore = useLoadingStore()
+    loadingStore.stopLoading()
+
     const status = error.response?.status
     const originalRequest = error.config
 
